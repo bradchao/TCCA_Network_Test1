65bacb17785106ba2802452da9b06d37a01642a6
bradchao <brad@brad.tw>
2020-09-19 09:47:35 +0800

    2020091902

--- app/src/main/java/tw/org/tcca/apps/test1/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test1/MainActivity.java
@@ -6,7 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+import static java.net.NetworkInterface.getNetworkInterfaces;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getMyIP();
+        getMyIPV2();
     }
 
     private void getMyIP(){
         new Thread(){
             @Override
             public void run() {
-                super.run();
                 try {
                     String ip = InetAddress.getLocalHost().getHostAddress();
                     Log.v("bradlog", ip);
@@ -30,6 +34,27 @@ public class MainActivity extends AppCompatActivity {
                     Log.v("bradlog", e.toString());
                 }
 
+            }
+        }.start();
+    }
+
+    private void getMyIPV2(){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Enumeration<NetworkInterface> ifs =  NetworkInterface.getNetworkInterfaces();
+                    while (ifs.hasMoreElements()){
+                        NetworkInterface iff = ifs.nextElement();
+                        Enumeration<InetAddress> ips =  iff.getInetAddresses();
+                        while (ips.hasMoreElements()){
+                            InetAddress ip = ips.nextElement();
+                            Log.v("bradlog", ip.getHostAddress());
+                        }
+                    }
+                } catch (SocketException e) {
+                    Log.v("bradlog", e.toString());
+                }
             }
         }.start();
     }
